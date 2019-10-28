e84a7be9f597c421a34257b3e5135a775ccad9f6
Nazarii Trokhymchenko <TROHYMCHENKO@GMAIL.COM>
2019-10-28 20:58:19 +0200

    Add Message resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_28_182324) do
+ActiveRecord::Schema.define(version: 2019_10_28_184317) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
