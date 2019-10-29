a7dd07610190acf98f1db091a491f59b824546e5
Nazarii Trokhymchenko <TROHYMCHENKO@GMAIL.COM>
2019-10-29 21:06:22 +0200

    Enable Flash messages display

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,3 +1,6 @@
-document.on("turbolinks:load", function() {
-  $(".ui.dropdown").dropdown()
+$(document).on("turbolinks:load", function() {
+  $(".ui.dropdown").dropdown();
+  $(".message .close").on("click", function() {
+    $(this).closest(".message").transition("fade");
+  });
 })
