c7a8bf977e441fda6dcdd970fe02af0c25dd923f
Nazarii Trokhymchenko <TROHYMCHENKO@GMAIL.COM>
2019-10-30 21:03:34 +0200

    Add auto scrolling to chat window

--- app/javascript/packs/chatroom.coffee
+++ app/javascript/packs/chatroom.coffee
@@ -9,4 +9,5 @@ App.chatroom = App.cable.subscriptions.create "ChatroomChannel",
     #Called when the subscription has been terminated by the serveer
 
   received: (data) ->
-    alert data.foo
+    $('#message-container').append data.mod_message
+    scroll_bottom()
--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,6 +1,14 @@
+
+scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
 $(document).on("turbolinks:load", function() {
   $(".ui.dropdown").dropdown();
   $(".message .close").on("click", function() {
     $(this).closest(".message").transition("fade");
   });
+  scroll_bottom();
 })
